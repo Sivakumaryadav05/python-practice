6f6077c0f53d26beab52bba08993cb8751791c59
Siva kumar yadav <bangarisivakumar5@gmail.com>
2026-06-04 14:07:51 +0530

    Add files via upload
    Added basic python programs

---
+++ day _1.py
@@ -0,0 +1,59 @@
+# #1st one
+# print("Let's start the challenge,I will stay consistent")
+''''''
+#2nd
+# n=str(input("enter your name:"))
+# print("welcome",n)
+''''''
+# 3rd one
+# n=int(input())
+# m=int(input())
+# sum=n+m
+# print(sum)
+''''''
+# 4th one
+# n=int(input())
+# m=int(input())
+# remainder=n%m
+# print(remainder)
+''
+#5th one
+# n=int(input())
+# m=int(input())
+# u=int(input())
+# avg=n+m+u/3
+# print(avg)
+''''''
+#6th one
+# n=int(input())
+# m=int(input())
+# n,m=m,n
+# print(n,m)
+''''''
+#7th one
+# s1=int(input("enter s1 marks"))
+# s2=int(input("enter s2 marks"))
+# s3=int(input("enter s3 marks"))
+# s4=int(input("enter s4 marks"))
+# s5=int(input("enter s5 marks"))
+# total=s1+s2+s3+s4+s5
+# percentage=total/500*100
+# print(total)
+# print(percentage)
+''
+#8th one
+# temp=float(input("enter the temparature in celsius"))
+# fahrenheit=9/5*temp+32
+# print(fahrenheit,"^f")
+''''''
+#9thone
+# temp=int(input("enter the temparature in fahrenheit"))
+# celsius=9/5(temp-32)
+# print(celsius,"^c")
+''''''
+#10th one
+l=int(input("enter the length"))
+b=int(input("enter the breadth"))
+h=int(input("enter the height"))
+volume=l*b*h
+print("tha volume of a cuboid is",volume)
